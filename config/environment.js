543e50280987fa789a5f9dd50136cbea45dd580c
kuldip singh rai <krai@ciena.com>
2016-04-22 08:30:24 -0400

    Try fixing scss imports

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,7 @@
-'use strict'
+'use strict';
 
-module.exports = function (/* environment, appConfig */) {
-  return { }
-}
+module.exports = function(/* environment, appConfig */) {
+	return {
+		podModulePrefix: 'frost-sidebar/pods'
+	};
+};
--- tests/dummy/app/pods/demo/controller.js
+++ tests/dummy/app/pods/demo/controller.js
@@ -1,4 +1,22 @@
-import Ember from 'ember'
+import Ember from 'ember';
 
 export default Ember.Controller.extend({
-})
+  actions: {
+    tabSelected(tab) {
+      this.set('selectedTab', tab);
+    }
+  },
+
+  //frost-doc stuff
+  general: {
+    alias: 'general',
+    template: `
+      {{#frost-sidebar class="demo-instance"}}
+            Test content
+        {{#frost-checkbox-array id="basic" }}
+          {{#frost-checkbox id="basic_value-A" value="A" }}A{{/frost-checkbox}}
+        {{/frost-checkbox-array}}
+      {{/frost-sidebar}}
+      `
+  }
+});
