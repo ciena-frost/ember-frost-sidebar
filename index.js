9e16ba56afb729b0a15fb91359539d07221956c7
Jeremy Brown <jeremy@notmessenger.com>
2018-04-13 13:53:01 -0500

    New SVG implementation

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 /* eslint-env node */
 'use strict'
 
+const {setSvgConfiguration} = require('ember-frost-core/utils/frost-icon-svg')
+
 module.exports = {
-  name: 'ember-frost-sidebar'
+  name: 'ember-frost-sidebar',
+
+  included: function () {
+    this.app = this._findHost.call(this) // eslint-disable-line no-useless-call
+
+    // Set ember-cli-svgstore options so that consuming applications don't have to
+    setSvgConfiguration.call(this, 'frost-fields')
+
+    this._super.included.apply(this, arguments)
+  }
 }
